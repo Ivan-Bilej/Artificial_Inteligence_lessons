bfab2b27bf745145f0d6c1b065bdbb8e2e387286
Ivan <ivan.bilej25@gmail.com>
2024-02-22 10:52:53 +0100

    Completed process and resolved issue of why the simulation never ends

--- Second_lesson/Second_HW.py
+++ Second_lesson/Second_HW.py
@@ -15,7 +15,11 @@ def create_map(max_rows: int, max_cols: int):
 
 
 def agent_unhappy(total_count: int, alike_count: int, tolerance: float):
-    return True if alike_count / total_count <= tolerance else False
+
+    if alike_count == 0 or total_count == 0:
+        return True
+    else:
+        return True if alike_count / total_count <= tolerance else False
 
 
 def get_agent_surroundings(rep: list, row: int, col: int):
@@ -25,14 +29,16 @@ def get_agent_surroundings(rep: list, row: int, col: int):
 
     min_row = row - 1
     min_col = col - 1
-    max_row = row + 1 if row + 1 <= len(rep) else 0
-    max_col = col + 1 if col + 1 <= len(rep[row]) else 0
+    max_row = row + 2 if row + 2 <= len(rep) else 1
+    max_col = col + 2 if col + 2 <= len(rep[row]) else 1
 
     for surr_row in range(min_row, max_row):
         for surr_col in range(min_col, max_col):
-            if rep[surr_row, surr_col] == agent and surr_row != row and surr_col != col:
+            if rep[surr_row, surr_col] == agent and (surr_row != row or surr_col != col):
                 alike_agent_count += 1
-            total_agent_count += 1
+            if (rep[surr_row, surr_col] in agent1_list or rep[surr_row, surr_col] in agent2_list) and \
+                    (surr_row != row or surr_col != col):
+                total_agent_count += 1
 
     return alike_agent_count, total_agent_count
 
@@ -61,16 +67,17 @@ def step(rep: list):
 
     move_ind = rnd.choice(agents_to_move)
     free_ind = rnd.choice(free_spaces)
-    #move_ind = rng.choice(len(agents_to_move), 1, replace=False)[0]
-    #free_ind = rng.choice(len(free_spaces), 1, replace=False)[0]
+    # move_ind = rng.choice(len(agents_to_move), 1, replace=False)[0]
+    # free_ind = rng.choice(len(free_spaces), 1, replace=False)[0]
 
-    #print("Before:\n", rep)
-    #print("Agent position:", move_ind)
-    #print("Free space position:", free_ind)
+    # print("Before:\n", rep)
+    # print("Agent position:", move_ind)
+    # print("Free space position:", free_ind)
     rep[free_ind[0], free_ind[1]] = rep[move_ind[0], move_ind[1]]
     rep[move_ind[0], move_ind[1]] = 0
 
-    #print("After:\n", rep)
+    # print("After:\n", rep)
+
 
 
 def plot(rep: list, plot_ax):
@@ -78,7 +85,7 @@ def plot(rep: list, plot_ax):
     plot_ax.imshow(rep, cmap=custom_plot_colors, interpolation="nearest")
     plot_ax.set_aspect("equal")
     plt.draw()
-    plt.pause(0.05)
+    plt.pause(0.005)
 
 
 def main():
@@ -86,7 +93,7 @@ def main():
     numpy_torus = np.array(start_torus, dtype=int)
 
     unhappy_agents, free_spaces = find_free_space_and_unhappy_agents(numpy_torus)
-    while unhappy_agents != 0:
+    while len(unhappy_agents) != 0:
         step(numpy_torus)
         plot(numpy_torus, plot_ax)
         unhappy_agents, free_spaces = find_free_space_and_unhappy_agents(numpy_torus)
@@ -101,7 +108,7 @@ if __name__ == "__main__":
     agent1_list = [1]
     agent1_tolerance = 0.4
     agent2_list = [2]
-    agent2_tolerance = 0.6
+    agent2_tolerance = 0.4
 
     rng = np.random.default_rng(123456)
     fig = plt.figure()
