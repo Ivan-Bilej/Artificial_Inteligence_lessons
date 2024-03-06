c9d6664b93615251971e64b44711429f9ea377a8
Ivan <ivan.bilej25@gmail.com>
2024-03-07 00:09:11 +0100

    Script that will create an n amount of individuals in with k amount of positions that hold value from in range(0, 1). After l amount of generation it will try to reach the best possible fitness (evaluation of the individual).

---
+++ Fourth_lesson/Fourth_HW.py
@@ -0,0 +1,162 @@
+import numpy as np
+import random as rnd
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from deap import base, creator, tools, algorithms
+
+
+def plot_terrain(t):
+    fig, ax = plt.subplots()
+
+    x = range(len(t))
+    sea = [0.5 for i in range(len(t))]
+
+    print(t)
+    ax.fill_between(x, sea, color="turquoise")
+    ax.fill_between(x, t, color="sandybrown")
+    ax.axis("off")
+    plt.draw()
+
+
+def plot_statistics(mean, maximum):
+    fig, ax = plt.subplots()
+
+    ax.plot(range(max_generations + 1), mean, label="mean")  # 0.tá generace zvlášť
+    ax.plot(range(max_generations + 1), maximum, label="max")
+
+    ax.legend()
+    plt.draw()
+
+
+def count_lakes_and_peaks_and_underwater(terrain: list):
+    lake_start = 0.0
+    lake_count = 0
+    peak_start = 0.0
+    peak_count = 0
+    underwater_count = 0
+
+    for point in terrain:
+        if point <= 0.5 and lake_start == 0.0:
+            lake_start = point
+        elif point >= 0.5 and lake_start != 0.0:
+            lake_count += 1
+            lake_start = 0.0
+
+        if point >= 0.5 and peak_start == 0.0:
+            peak_start = point
+        elif point < 0.5 and peak_start != 0.0:
+            peak_count += 1
+            peak_start = 0.0
+
+        if point < 0.5:
+            underwater_count += 1
+
+    if lake_start != 0.0:
+        lake_count += 1
+    elif peak_start != 0.0:
+        peak_count += 1
+
+    return lake_count, peak_count, underwater_count
+
+
+def evaluate(individual):
+    lakes, peaks, underwater_count = count_lakes_and_peaks_and_underwater(individual)
+    variability = max(individual) - min(individual)
+    underwater_percentage = underwater_count / len(individual)
+
+    fitness = 1.0 * abs(number_of_lakes - lakes) + \
+              1.0 * abs(number_of_peaks - peaks) + \
+              1.0 * abs(terrain_variability - variability) + \
+              1.0 * abs(underwater_perc - underwater_percentage) + \
+              1.0 * abs(lake_size - underwater_count) + \
+              1.0 * abs(lake_depth - min(individual))
+
+    fitness = 1 / fitness
+    print(fitness)
+    return (fitness,)
+
+
+def create_toolbox():
+    def custom_attr_float():
+        return round(rnd.random(), 1)
+
+    # Sets the fitness weight
+    creator.create("FitnessMax", base.Fitness, weights=fitness_weights)
+    # Creates an object named Individual that holds list of data about itself
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    toolbox = base.Toolbox()
+    # Generate a random number from 0 to 1
+    toolbox.register("attr_float", custom_attr_float)
+    # Create an individual containing those numbers. Maximum amount of number mentioned by n
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
+                     toolbox.attr_float, n=max_data_length)
+    # Creates a population from Individuals
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    # Register function evaluate into toolbox
+    toolbox.register("evaluate", evaluate)
+    # Register crossover (mating) of 2 individuals
+    toolbox.register("mate", tools.cxOnePoint)
+    # Registers mutation of the individual with thw lowest and highest possible value % of the mutation
+    toolbox.register("mutate", tools.mutUniformInt, low=0, up=1, indpb=0.05)
+    # Select up to tournsize amount of Individuals via tournament
+    toolbox.register("select", tools.selTournament, tournsize=3)
+
+    return toolbox
+
+
+def set_up_statistics():
+    stats = tools.Statistics(key=lambda ind: ind.fitness.values)
+    stats.register("mean", np.mean)
+    stats.register("max", np.max)
+
+    return stats
+
+
+def main():
+    toolbox = create_toolbox()
+    stats = set_up_statistics()
+    hof = tools.HallOfFame(1)
+
+    pop = toolbox.population(n=20)
+    print(pop)
+    plot_terrain(pop[0])
+    finalpop, logbook = algorithms.eaSimple(pop, toolbox,
+                                            cxpb=crossover_percent,
+                                            mutpb=mutation_percent,
+                                            ngen=max_generations,
+                                            stats=stats,
+                                            halloffame=hof)
+    print(finalpop)
+    print(logbook)
+    print(hof)
+
+    mean, maximum = logbook.select("mean", "max")
+    # Ukázka druhé populace
+
+    plot_terrain(finalpop[len(finalpop) - 1])
+    plot_statistics(mean, maximum)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_data = [1.0, 0.2, 0.5, 0.6, 0.2, 0.7, 0.8, 0.5, 0.3, 0.5, 0.4, 0.5, 0.8]
+
+    max_generations = 50
+    max_data_length = 10
+    crossover_percent = 0.7
+    mutation_percent = 0.4
+    identity_info = 5
+    fitness_weights = (1.0,)
+
+    # Criteria
+    number_of_lakes = 2
+    number_of_peaks = 3
+    terrain_variability = 0.65
+    underwater_perc = 0.3
+    lake_size = 2
+    lake_depth = 0.3
+    # ... can be added more later on
+
+    main()
